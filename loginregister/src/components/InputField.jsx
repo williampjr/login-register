0b1a794a1309105be38de70bc12c73b6e04856aa
jr-will <pjrwilliam7@gmail.com>
2026-05-16 01:50:10 -0300

    fix: change param name

--- loginregister/src/components/InputField.jsx
+++ loginregister/src/components/InputField.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
-export function InputField({ label, type, autoComplete, name }) {
+export function InputField({
+  label,
+  type,
+  autoComplete,
+  name,
+  error,
+  register,
+}) {
   return (
     <div className="mb-4">
       <label htmlFor={name} className="block mb-1 font_medium">
@@ -10,7 +17,7 @@ export function InputField({ label, type, autoComplete, name }) {
         type={type}
         id={name}
         autoComplete={autoComplete}
-        {...Register(name)}
+        {...register(name)}
         className={`w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 ${error ? "border-red-500 focus:ring-red-400" : "border-gray-300 focus:ring-blue-400"}`}
       />
       {error && <p className="text-red-500 text-sm mt-1">{error.message}</p>}
