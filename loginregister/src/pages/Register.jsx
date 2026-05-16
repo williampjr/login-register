ad1cf170d18c3629296f6bc1c23757f582021ba1
jr-will <pjrwilliam7@gmail.com>
2026-05-16 02:05:41 -0300

    feat: add input fields in register form

--- loginregister/src/pages/Register.jsx
+++ loginregister/src/pages/Register.jsx
@@ -1,5 +1,55 @@
 import React from "react";
+import { InputField } from "../components/InputField";
+import { Link } from "react-router";
 
 export function Register() {
-  return <div>Register</div>;
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <form className="bg-white p-8 rounded-2xl shadow-md w-96">
+        <h2 className="text-2xl font-bold mb-6 text-center">Sign up</h2>
+        <InputField
+          label="Name"
+          id="name"
+          autoComplete="name"
+          name="name"
+          register={() => {}}
+        />
+        <InputField
+          label="Email"
+          id="email"
+          autoComplete="email"
+          name="email"
+          register={() => {}}
+        />
+        <InputField
+          label="Password"
+          id="password"
+          autoComplete="password"
+          name="password"
+          type="password"
+          register={() => {}}
+        />
+        <InputField
+          label="Confirm password"
+          id="password"
+          autoComplete="password"
+          name="password"
+          type="password"
+          register={() => {}}
+        />
+        <button
+          type="submit"
+          className="w-full bg-green-500 text-white py-2 rounded-lg hover:bg-green-600 transition "
+        >
+          sign in
+        </button>
+        <p className="mt-4 text-center text-sm">
+          Already have an account?{" "}
+          <Link to="/login" className="text-blue-500 hover:underline">
+            Sign in
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
 }
